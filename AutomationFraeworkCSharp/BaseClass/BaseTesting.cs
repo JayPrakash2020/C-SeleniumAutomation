ad4038b28ab7d5d93de16bed17fd6f22ade0b2c8
JayPrakash2020 <jayprakashutw@gmail.com>
2023-09-21 20:47:16 +0530

    Latest Changes Done
    Done

---
+++ AutomationFraeworkCSharp/BaseClass/BaseTesting.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationFraeworkCSharp.BaseClass
+{
+    public class BaseTesting
+    {
+        public IWebDriver driver;
+       [OneTimeSetUp] 
+        public void SetUp() 
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+           
+        }
+        [OneTimeTearDown]
+        public void Flush()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
